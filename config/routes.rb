c9570e4608dd6ed5a3184f836799e5e5a4eacd70
Mohammed Ali <m.ali.overthrow@gmail.com>
2023-02-04 11:42:39 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/sign-in", to: "khateebs#sign_in"
+  post "/sign-up", to: "khateebs#sign_up"
+  get "/khateebs/:username", to: "khateebs#show"
+  put "/khateebs/:username/avatar", to: "khateebs#change_avatar"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/reminders", to: "reminders#show_feed"
+  reminder "/reminders", to: "reminders#create"
+  reminder "/reminders/:reminder_id", to: "reminders#like"
+  delete "/reminders/:reminder_id", to: "reminders#unlike"
+
+  post "/follow", to: "follows#create"
+  delete "/follow", to: "follows#destroy"
+
 end
